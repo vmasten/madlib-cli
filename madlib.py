080afcba2744acf6ad4ec59dfac5c741a2808d8b
Vince Masten <vmasten@me.com>
2018-12-02 11:51:03 -0800

    functionality complete, testing not

--- madlib.py
+++ madlib.py
@@ -4,8 +4,6 @@ parts of speech and combining them into a silly story.
 
 import re
 
-regex = r"\{(.*?)\}"
-
 
 def greeting():
     """Greet the user and provide instructions."""
@@ -15,43 +13,78 @@ def greeting():
 
 
 def read_file(path):
-    """Read a file and return the data as a giant string."""
+    """Read path and return the data as a giant string."""
     with open(path, 'r') as rf:
-        data = rf.read()
-        return data
+        return rf.read()
 
 
-def parse_matches(data):
-    """Use regex to find all parts of speech to modify."""
-    matches = re.findall(regex, data)
-    return matches
+def write_file(path, out):
+    """Write a file back to the given path."""
+    with open(path, 'w') as wf:
+        return wf.write(out)
 
 
-def change_matches(matches):
-    """Ask the user for input to be inserted back into the story."""
-    user_in = {}
-    for item in matches:
-        print('Please give me an', item.lower(), ': ', end='')
-        new = input()
-        user_in[item] = new
-    return user_in
+def get_keys(format_string):
+    """Get parts of speech to be modified."""
+    key_list = []
+    end = 0
+
+    repetitions = format_string.count('{')
+    for i in range(repetitions):
+        start = format_string.find('{', end) + 1
+        end = format_string.find('}', start)
+        key = format_string[start:end]
+        key_list.append(key)
+
+    return key_list
 
 
-def reinsert_matches(user_in):
-    """Recombines the matches into the story. Currently broken."""
-    data = open('test.txt')
-    for line in data:
-        line.replace('{', '')
-        line.replace('}', '')
-    # to_change = re.findall(regex, data)
-    # new = re.compile(r'[{}]', flags=re.UNICODE)
-        print(line)
+def remove_keys(format_string):
+    """Remove the parts of speech from the text for modification."""
+    regex = r"\{.*?\}"
+    output = re.sub(regex, '{}', format_string)
+    return output
+
+
+def parse(raw):
+    """
+    Parse input for further processing.
+
+        get_keys is called to get the parts of speech to be modified
+        remove_keys is called to strip the parts of speech to be replaced
+    """
+    prompts = get_keys(raw)
+    stripped = remove_keys(raw)
+    return prompts, stripped
+
+
+def add_response(prompt, responses):
+    if prompt[0] == 'A' or 'I':
+        # In case the user is asked for an adjective or interjection
+        # Can't help but fix grammar whenever possible
+        response = input(f'Enter an {prompt}: ')
+        responses.append(response)
+    else:
+        response = input(f'Enter a {prompt}: ')
+
+
+def get_responses(prompts):
+    responses = []
+
+    for prompt in prompts:
+        add_response(prompt, responses)
+
+    return responses
+
+def output_story(raw):
+    prompts, stripped = parse(raw)
+    responses = get_responses(prompts)
+    story = stripped.format(*responses)
+    return story
 
 
 if __name__ == "__main__":
     greeting()
-    story = read_file('test.txt')
-    changes = parse_matches(story)
-    madlibs = change_matches(changes)
-    print(madlibs)
-    reinsert_matches(madlibs)
+    raw = read_file('test.txt')
+    story = output_story(raw)
+    write_file('madlib_complete.txt', story)
--- test_madlib.py
+++ test_madlib.py
@@ -1,7 +1,63 @@
-"""Tests for input/output of madlib.py. Horribly incomplete."""
-from madlib import read_file
+"""Tests for input/output of madlib.py."""
+import madlib
+import pytest
+import sys
 
 
 def test_read_file_exists():
     """Test whether the read_file function exists."""
-    assert read_file
+    assert madlib.read_file
+
+
+def test_write_file():
+    """Test whether write_file works properly."""
+    madlib.write_file('./foo.txt', 'foo')
+
+    with open('./foo.txt') as f:
+        assert f.read() == 'foo'
+
+
+def test_get_keys():
+    """Tests whether get_keys can get parts of speech from simple input."""
+    raw = 'It was a {Adjective} and {Adjective} {Noun}'
+    expected = ['Adjective', 'Adjective', 'Noun']
+    assert madlib.get_keys(raw) == expected
+
+
+def test_remove_keys():
+    """Tests the removal of keys from a given string."""
+    raw = 'It was a {Adjective} and {Adjective} {Noun}'
+    expected = 'It was a {} and {} {}'
+    assert madlib.remove_keys(raw) == expected
+
+
+def test_add_response_vowel():
+    sys.stdin = 'annoying'
+    responses = []
+    raw = 'Adjective'
+    madlib.add_response(raw, responses)
+
+
+
+def test_parse():
+    """Test whether parse works with simple input."""
+    prompts, stripped = madlib.parse('It was a {Adjective} and {Adjective} {Noun}')
+    assert prompts == ['Adjective', 'Adjective', 'Noun']
+    assert stripped == 'It was a {} and {} {}'
+
+
+def test_output_story():
+    """Test whether story output is well-formed."""
+
+    raw = 'It was a {Adjective} and {Adjective} {Noun}'
+
+    input_values = iter(['dark', 'stormy', 'night'])
+
+    def mock_input(s):
+        return next(input_values)
+
+    madlib.input = mock_input
+
+    story = madlib.output_story(raw)
+
+    assert story == 'It was a dark and stormy night'
